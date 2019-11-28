5f4161a0f9d3e8d70d198e63615ed87c9707f813
Jean Carlo Mata <jean.cms@hotmail.es>
2019-11-28 07:29:28 -0600

    Refactor

--- front-end/src/components/Add.js
+++ front-end/src/components/Add.js
@@ -3,176 +3,185 @@ import {Button} from 'react-bootstrap';
 import Modal from 'react-modal';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+
 class Add extends React.Component {
-constructor() {
-      super();
-this.state = {
-        reason: '',
-        amount: '',
-        month: '',
-        year: '',
-        messageFromServer: '',
-        modalIsOpen: false
-      }
-this.handleSelectChange = this.handleSelectChange.bind(this);
-      this.onClick = this.onClick.bind(this);
-      this.handleTextChange = this.handleTextChange.bind(this);
-      this.insertNewExpense = this.insertNewExpense.bind(this);
-      this.openModal = this.openModal.bind(this);
-      this.closeModal = this.closeModal.bind(this);
+
+  constructor() {
+    super();
+    this.state = {
+      reason: '',
+      amount: '',
+      month: '',
+      year: '',
+      messageFromServer: '',
+      modalIsOpen: false
     }
-openModal() {
-      this.setState({
-        modalIsOpen: true
-      });
-    }
-closeModal() {
-      this.setState({
-        modalIsOpen: false,
-        reason: '',
-        amount: '',
-        month: 'Jan',
-        year: 2016,
-        messageFromServer: ''
-      });
-    }
-componentDidMount() {
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.insertNewExpense = this.insertNewExpense.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({
+      modalIsOpen: true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false,
+      reason: '',
+      amount: '',
+      month: 'Jan',
+      year: 2016,
+      messageFromServer: ''
+    });
+  }
+
+  componentDidMount() {
     if(this.props.selectedMonth === 'All'){
       this.setState({
         month: 'Jan'
       });
-    }else{
+    }
+    else{
       this.setState({
         month: this.props.selectedMonth
       });
     }
+    this.setState({
+      year: this.props.selectedYear
+    });
+  }
 
+  componentWillReceiveProps(nextProps) {
+    if(this.props.selectedMonth === 'All'){
       this.setState({
-        year: this.props.selectedYear
+        month: 'Jan'
       });
     }
-componentWillReceiveProps(nextProps){
-      if(this.props.selectedMonth === 'All'){
-        this.setState({
-          month: 'Jan'
-        });
-      }else{
-        this.setState({
-          month: this.props.selectedMonth
-        });
-      }
-
+    else {
       this.setState({
-        year:nextProps.selectedYear
-      })
+        month: this.props.selectedMonth
+      });
     }
-handleSelectChange(e) {
-      if (e.target.name === 'month') {
-        this.setState({
-          month: e.target.value
-        });
-      }
-      if (e.target.name === 'year') {
-        this.setState({
-          year: e.target.value
-        });
-      }
+    this.setState({
+      year:nextProps.selectedYear
+    })
+  }
+
+  handleSelectChange(e) {
+    if (e.target.name === 'month') {
+      this.setState({
+        month: e.target.value
+      });
     }
-onClick(e) {
-      this.insertNewExpense(this);
+    if (e.target.name === 'year') {
+      this.setState({
+        year: e.target.value
+      });
     }
-insertNewExpense(e) {
+  }
+
+  onClick(e) {
+    this.insertNewExpense(this);
+  }
+
+  insertNewExpense(e) {
     var expense = {
       reason: e.state.reason,
       amount: e.state.amount,
       month: e.state.month,
       year: e.state.year
     }
-      axios.post('http://localhost:8080/vacations', expense).then(function(response) {
-        e.setState({
-          messageFromServer: response.data
-        });
+    axios.post('http://localhost:8080/vacations', expense).then(function(response) {
+      e.setState({
+        messageFromServer: response.data
+      });
+    });
+  }
+
+  handleTextChange(e) {
+    if (e.target.name === "reason") {
+      this.setState({
+        reason: e.target.value
       });
     }
-handleTextChange(e) {
-      if (e.target.name === "reason") {
-        this.setState({
-          reason: e.target.value
-        });
-      }
-if (e.target.name === "amount") {
-        this.setState({
-          amount: e.target.value
-        });
-      }
+    if (e.target.name === "amount") {
+      this.setState({
+        amount: e.target.value
+      });
     }
-render() {
-   if(this.state.messageFromServer === ''){
+  }
+
+  render() {
+    if(this.state.messageFromServer === '') {
       return (
         <div>
-      <Button bsStyle="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
+          <Button bsStyle="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
           <Modal
             isOpen={this.state.modalIsOpen}
             onRequestClose={this.closeModal}
             contentLabel="Add Expense"
-       className="Modal">
-<Link to={{pathname: '/', search: '?month='+this.state.month+'&year='+this.state.year }} style={{ textDecoration: 'none' }}>
-       <Button bsStyle="danger" size="mini" onClick={this.closeModal}><span className="closebtn glyphicon glyphicon-remove"></span></Button>
-      </Link><br/>
-<fieldset>
-       <label for="reason">reason:</label><input type="text" id="reason" name="reason" value={this.state.reason} onChange={this.handleTextChange}></input>
-       <label for="amount">Amount:</label><input type="number" id="amount" name="amount" value={this.state.amount} onChange={this.handleTextChange}></input>
-       <label for="month">Month:</label><select id="month" name="month" value={this.state.month} onChange={this.handleSelectChange}>
-            <option value="Jan" id="Jan">January</option>
-            <option value="Feb" id="Feb">Febrary</option>
-            <option value="Mar" id="Mar">March</option>
-            <option value="Apr" id="Apr">April</option>
-            <option value="May" id="May">May</option>
-            <option value="Jun" id="Jun">June</option>
-            <option value="Jul" id="Jul">July</option>
-            <option value="Aug" id="Aug">August</option>
-            <option value="Sep" id="Sep">September</option>
-            <option value="Oct" id="Oct">October</option>
-            <option value="Nov" id="Nov">November</option>
-            <option value="Dec" id="Dec">December</option>
-         </select>
-       <label for="year">Year:</label><select id="year" name="year" value={this.state.year} onChange={this.handleSelectChange}>
-            <option value="2015" id="17">2015</option>
-            <option value="2016" id="17">2016</option>
-            <option value="2017" id="17">2017</option>
-            <option value="2018" id="18">2018</option>
-            <option value="2019" id="19">2019</option>
-            <option value="2020" id="20">2020</option>
-         </select>
-      </fieldset>
-<div className='button-center'>
-        <br/>
-        <Button bsStyle="success" size="small" onClick={this.onClick}>Add New Expense</Button>
-       </div>
+            className="Modal">
+            <Link to={{pathname: '/', search: '?month='+this.state.month+'&year='+this.state.year }} style={{ textDecoration: 'none' }}>
+              <Button bsStyle="danger" size="mini" onClick={this.closeModal}><span className="closebtn glyphicon glyphicon-remove"></span></Button>
+            </Link>
+            <br/>
+            <fieldset>
+              <label for="reason">reason:</label><input type="text" id="reason" name="reason" value={this.state.reason} onChange={this.handleTextChange}></input>
+              <label for="amount">Amount:</label><input type="number" id="amount" name="amount" value={this.state.amount} onChange={this.handleTextChange}></input>
+              <label for="month">Month:</label>
+              <select id="month" name="month" value={this.state.month} onChange={this.handleSelectChange}>
+                <option value="Jan" id="Jan">January</option>
+                <option value="Feb" id="Feb">Febrary</option>
+                <option value="Mar" id="Mar">March</option>
+                <option value="Apr" id="Apr">April</option>
+                <option value="May" id="May">May</option>
+                <option value="Jun" id="Jun">June</option>
+                <option value="Jul" id="Jul">July</option>
+                <option value="Aug" id="Aug">August</option>
+                <option value="Sep" id="Sep">September</option>
+                <option value="Oct" id="Oct">October</option>
+                <option value="Nov" id="Nov">November</option>
+                <option value="Dec" id="Dec">December</option>
+              </select>
+              <label for="year">Year:</label>
+              <select id="year" name="year" value={this.state.year} onChange={this.handleSelectChange}>
+                <option value="2015" id="17">2015</option>
+                <option value="2016" id="17">2016</option>
+                <option value="2017" id="17">2017</option>
+                <option value="2018" id="18">2018</option>
+                <option value="2019" id="19">2019</option>
+                <option value="2020" id="20">2020</option>
+              </select>
+            </fieldset>
+            <div className='button-center'>
+              <br/>
+              <Button bsStyle="success" size="small" onClick={this.onClick}>Add New Expense</Button>
+            </div>
           </Modal>
         </div>
       )
-   }
-   else{
-    return (
-     <div>
-       <Button bsStyle="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
-       <Modal
-        isOpen={this.state.modalIsOpen}
-        onAfterOpen={this.afterOpenModal}
-        onRequestClose={this.closeModal}
-        contentLabel="Add Expense"
-        className="Modal">
-<div className='button-center'>
-        <h3>{this.state.messageFromServer}</h3>
-        <Link to={{pathname: '/', search: '?month='+this.state.month+'&year='+this.state.year}} style={{ textDecoration: 'none' }}>
-         <Button bsStyle="success" size="mini" onClick={this.closeModal}>Close the Dialog</Button>
-        </Link>
-       </div>
-      </Modal>
-       </div>
-     )
     }
-   }
+    else{
+      return (
+        <div>
+          <Button bsStyle="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
+          <Modal isOpen={this.state.modalIsOpen} onAfterOpen={this.afterOpenModal} onRequestClose={this.closeModal} contentLabel="Add Expense" className="Modal">
+            <div className='button-center'>
+              <h3>{this.state.messageFromServer}</h3>
+              <Link to={{pathname: '/', search: '?month='+this.state.month+'&year='+this.state.year}} style={{ textDecoration: 'none' }}>
+                <Button bsStyle="success" size="mini" onClick={this.closeModal}>Close the Dialog</Button>
+              </Link>
+            </div>
+          </Modal>
+        </div>
+      )
+    }
+  }
 }
 export default Add;
